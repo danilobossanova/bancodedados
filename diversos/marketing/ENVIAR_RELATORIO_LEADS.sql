2d29e7d2f63f637eab85b8c297aedf339e6ee583
Danilo Fernando <danilo.bossanova@hotmail.com>
2024-09-13 10:10:22 -0300

    Update ENVIAR_RELATORIO_LEADS.sql
    Melhoria na legibilidade

--- diversos/marketing/ENVIAR_RELATORIO_LEADS.sql
+++ diversos/marketing/ENVIAR_RELATORIO_LEADS.sql
@@ -6,6 +6,9 @@ CREATE OR REPLACE PROCEDURE ENVIAR_RELATORIO_LEADS AS
     v_count_atrasados NUMBER;
     v_relatorio     CLOB;
     v_link_contato  VARCHAR2(400);  -- Para construir o link HTML
+    
+    v_error_stack VARCHAR2(4000);
+    
 BEGIN
     -- Cursor para percorrer os gerentes distintos e seus respectivos emails
     FOR gerente_rec IN (
@@ -20,6 +23,7 @@ BEGIN
 
             -- Verifica se o email contém um formato inválido
             IF NOT REGEXP_LIKE(v_email_destino, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$') THEN
+                DBMS_OUTPUT.PUT_LINE('Email inválido para o gerente ' || v_gerente || ': ' || SQLERRM);
                 RAISE_APPLICATION_ERROR(-20001, 'Email inválido para o gerente ' || v_gerente || ': ' || v_email_destino);
             END IF;
 
@@ -29,10 +33,7 @@ BEGIN
             v_relatorio := v_relatorio || '----------------------------------------' || '<br>';
             v_relatorio := v_relatorio || 'Gerente: ' || v_gerente || CHR(10);
             v_relatorio := v_relatorio || '----------------------------------------' || '<br>';
-            v_relatorio := v_relatorio || RPAD('Data Criação', 15) || ' | ' 
-                                           || RPAD('URL do Lead', 40) || ' | ' 
-                                           || RPAD('Situação', 10) || ' | '
-                                           || 'Data Prevista Retorno <br>';
+            v_relatorio := v_relatorio || RPAD('Data Criação', 15) || ' | ' || RPAD('URL do Lead', 40) || ' | ' || RPAD('Situação', 10) || ' | ' || 'Data Prevista Retorno <br>';
             v_relatorio := v_relatorio || '----------------------------------------------------------------------------------------<br>';
 
             -- Inicializa contadores
@@ -51,10 +52,7 @@ BEGIN
                 v_link_contato := '<a href="' || lead_rec.URL || '">' || lead_rec.NOME_CONTATO || ' - ' || lead_rec.CIDADE || '</a>';
 
                 -- Acumula as informações do lead no relatório, incluindo o link HTML
-                v_relatorio := v_relatorio || RPAD(lead_rec.DATA_CRIACAO, 15) || ' | ' 
-                                               || RPAD(v_link_contato, 40) || ' | ' 
-                                               || RPAD(lead_rec.SITUACAO, 10) || ' | '
-                                               || lead_rec.DATA_PREVISTA_RETORNO || '<br>';
+                v_relatorio := v_relatorio || RPAD(lead_rec.DATA_CRIACAO, 15) || ' | ' || RPAD(v_link_contato, 40) || ' | ' || RPAD(lead_rec.SITUACAO, 10) || ' | ' || lead_rec.DATA_PREVISTA_RETORNO || '<br>';
 
                 -- Verifica se o lead está atrasado
                 IF lead_rec.SITUACAO = 'ATRASADO' THEN
@@ -78,19 +76,50 @@ BEGIN
             -- Define o assunto do email
             v_assunto := 'Relatório de Leads Abertos - ' || v_gerente;
 
-            -- Envia o relatório gerado por email
-            SEND_NOTIFICATION(
-                p_destinatario_usuario => 68,  -- Usuário destinatário (não especificado)
-                p_destinatario_grupo => NULL,    -- Grupo destinatário (não especificado)
-                p_assunto => v_assunto,          -- Assunto do email
-                p_mensagem => v_relatorio,       -- Conteúdo do email (relatório gerado)
-                p_prioridade => 2                -- Prioridade média
-            );
+            -- Verifica se existem leads atrasados
+            IF v_count_atrasados > 0 THEN
+                -- Envia a notificação
+                SEND_NOTIFICATION(
+                    p_destinatario_usuario => 68,  -- Usuário destinatário
+                    p_destinatario_grupo => NULL,  -- Grupo destinatário
+                    p_assunto => v_assunto,        -- Assunto da notificação
+                    p_mensagem => v_relatorio,     -- Conteúdo da notificação
+                    p_prioridade => 2              -- Prioridade média
+                );
+
+                -- Envia o email usando a procedure de email
+                SANKHYA.EMAIL(
+                    P_EMAIL =>  'danilo.fernando@grupocopar.com.br' /*v_email_destino*/,     -- Endereço de email do gerente
+                    P_TITULO => v_assunto,          -- Assunto do email
+                    P_MSGCLOB => v_relatorio        -- Conteúdo do email (HTML com <br>)
+                );
+                
+            ELSE
+                -- Caso não existam leads atrasados, envia notificação de "Parabéns"
+                SEND_NOTIFICATION(
+                    p_destinatario_usuario => 68,  -- Usuário destinatário
+                    p_destinatario_grupo => NULL,  -- Grupo destinatário
+                    p_assunto => v_assunto,        -- Assunto da notificação
+                    p_mensagem => 'Parabéns! Nenhum lead está atrasado!',  -- Mensagem
+                    p_prioridade => 2              -- Prioridade média
+                );
+
+                -- Envia o email de "Parabéns"
+                SANKHYA.EMAIL(
+                    P_EMAIL =>  'danilo.fernando@grupocopar.com.br', /*v_email_destino,*/     -- Endereço de email do gerente
+                    P_TITULO => v_assunto,          -- Assunto do email
+                    P_MSGCLOB => 'Parabéns! Nenhum lead está atrasado!'  -- Conteúdo do email
+                );
+            END IF;
+
+            COMMIT;
 
         EXCEPTION
             WHEN OTHERS THEN
-                -- Se houver algum erro ao processar esse gerente, exibe a mensagem de erro
-                DBMS_OUTPUT.PUT_LINE('Erro ao processar o gerente ' || v_gerente || ': ' || SQLERRM);
+                -- Captura o erro e exibe a mensagem
+                v_error_stack := DBMS_UTILITY.FORMAT_ERROR_STACK;
+                DBMS_OUTPUT.PUT_LINE('Erro ao processar o gerente ' || v_gerente || ':');
+                DBMS_OUTPUT.PUT_LINE(v_error_stack);
         END;
     END LOOP;
 END;

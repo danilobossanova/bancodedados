1d3a85e26ae408ca5582b4cb465d76e2e840c821
Danilo Fernando <danilo.bossanova@hotmail.com>
2024-11-01 17:47:36 -0300

    Update SP_GERARECONT_INVT_DCCO2.sql
    Refatoração

--- diversos/wms/botaoacao/SP_GERARECONT_INVT_DCCO2.sql
+++ diversos/wms/botaoacao/SP_GERARECONT_INVT_DCCO2.sql
@@ -5,91 +5,153 @@ CREATE OR REPLACE PROCEDURE SANKHYA."SP_GERARECONT_INVT_DCCO2" (
        P_MENSAGEM OUT VARCHAR2 -- Caso seja passada uma mensagem aqui, ela será exibida como uma informação ao usuário.
 ) AS
        
-       -- Declaração de variáveis para campos dos registros
-       FIELD_NUIVT NUMBER;
-       FIELD_SEQUENCIA NUMBER;
-       FIELD_CODPROD NUMBER;
-       FIELD_CODEND NUMBER;
-       
-       -- Variáveis para controle de tarefas e respostas
-       V_NUTAREFA NUMBER;
-       V_RESPOSTA VARCHAR2(400);
-       V_RESPOSTA_ITT VARCHAR2(400);
-       V_RESPOSTA_UXT VARCHAR2(400);
-       
-       -- Variáveis auxiliares para construção de mensagens
-       AUX_PRODUTOS_CTG_ABERTAS CLOB := NULL;
-       AUX_PRODUTOS_ADICIONADOS CLOB := NULL;
-       AUX_PRODUTOS_CONF_OK CLOB := NULL;
-       ERRO_ITT CLOB := NULL;
+        /* Tipos */
+    TYPE t_reg_produto IS RECORD (
+        nu_ivt      NUMBER,
+        sequencia   NUMBER,
+        cod_prod    NUMBER,
+        cod_end     NUMBER
+    );
+    
+    /* Cursor para processar produtos */
+    CURSOR c_produtos IS 
+        WITH produtos AS (
+            SELECT LEVEL AS indice
+            FROM DUAL 
+            CONNECT BY LEVEL <= p_qtd_linhas
+        )
+        SELECT 
+            ACT_INT_FIELD(p_id_sessao, indice, 'NUIVT') AS nu_ivt,
+            ACT_INT_FIELD(p_id_sessao, indice, 'SEQUENCIA') AS sequencia,
+            ACT_INT_FIELD(p_id_sessao, indice, 'CODPROD') AS cod_prod,
+            ACT_INT_FIELD(p_id_sessao, indice, 'CODEND') AS cod_end
+        FROM produtos;
+    
+    /* Variáveis */
+    v_reg_produto        t_reg_produto;
+    v_num_tarefa        NUMBER;
+    
+    v_resposta_itt      VARCHAR2(400);
+    v_resposta_uxt      VARCHAR2(400);
+    
+    v_produtos_abertos  VARCHAR2(32000);
+    v_produtos_conf_ok  VARCHAR2(32000);
+    v_produtos_add      VARCHAR2(32000);
+    
+    /* Constantes */
+    c_quebra_linha      CONSTANT VARCHAR2(10) := '<br>';
+    c_tipo_tarefa      CONSTANT NUMBER := 6;
+    c_status_ativo     CONSTANT VARCHAR2(1) := 'A';
+    c_flag_nao         CONSTANT VARCHAR2(1) := 'N';
        
        
        
 BEGIN
 
+        /* Inicialização das variáveis de mensagem */
+        v_produtos_abertos := NULL;
+        v_produtos_conf_ok := NULL;
+        v_produtos_add     := NULL; 
       
-       FOR I IN 1..P_QTDLINHAS -- Este loop permite obter o valor de campos dos registros envolvidos na execução.
-       LOOP                    -- A variável "I" representa o registro corrente.
+       /* Processamento de cada produto */
+        FOR r_produto IN c_produtos LOOP                   -- A variável "I" representa o registro corrente.
           
            -- Obtenção dos campos do registro corrente
-           FIELD_NUIVT := ACT_INT_FIELD(P_IDSESSAO, I, 'NUIVT');
+           /*FIELD_NUIVT := ACT_INT_FIELD(P_IDSESSAO, I, 'NUIVT');
            FIELD_SEQUENCIA := ACT_INT_FIELD(P_IDSESSAO, I, 'SEQUENCIA');
            FIELD_CODPROD := ACT_INT_FIELD(P_IDSESSAO, I, 'CODPROD');
-           FIELD_CODEND := ACT_INT_FIELD(P_IDSESSAO, I, 'CODEND');
+           FIELD_CODEND := ACT_INT_FIELD(P_IDSESSAO, I, 'CODEND');*/
 
 
-           -- Verificação se o produto possui tarefas de contagem em aberto
-           IF FN_CONTAGEM_ABERTAS_DCCO(FIELD_NUIVT, FIELD_CODPROD, FIELD_CODEND) > 0 THEN
-               AUX_PRODUTOS_CTG_ABERTAS := AUX_PRODUTOS_CTG_ABERTAS || '<br><b>Código:</b>' || FIELD_CODPROD;
-           ELSE
-               -- Verificação se a conferência do produto já está ok
-               IF FN_CONFERENCIA_OK_DCCO(FIELD_NUIVT, FIELD_CODPROD, FIELD_CODEND) = 1 THEN
-                   AUX_PRODUTOS_CONF_OK := AUX_PRODUTOS_CONF_OK || '<br><b>Código:</b>' || FIELD_CODPROD;
-               ELSE
-                   -- Geração de nova tarefa e inserção de registros relacionados
-                   STP_GERAR_NUTAREFA_DCCO(V_NUTAREFA);
-                   -- Inserção da tarefa de contagem
-                   INSERIR_TGWTAR_DCCO(V_NUTAREFA, 6, 'A', NULL, NULL, NULL, NULL, NULL, P_CODUSU, FIELD_NUIVT, NULL, NULL, 'N', 0, V_RESPOSTA);
-                   -- Registro do produto na tarefa de contagem
-                   INS_TGWITT_INVT_DCCO(V_NUTAREFA, FIELD_CODPROD, FIELD_CODEND, V_RESPOSTA_ITT);
-                   
-                   ERRO_ITT := ERRO_ITT || '<br>' || V_RESPOSTA_ITT;
-                   
-                   -- Definição de inventariante e registro na tarefa
-                   DefiniInventariante_DCCO(FIELD_NUIVT, FIELD_CODPROD, V_NUTAREFA, P_CODUSU, V_RESPOSTA_UXT);
-                   
-                   -- Registro dos produtos para exibir ao usuário
-                   AUX_PRODUTOS_ADICIONADOS := AUX_PRODUTOS_ADICIONADOS || '<br><br>Código: ' || FIELD_CODPROD || ' <b>NU TAREFA:</b> ' || V_NUTAREFA;
-                   
-               END IF;
-           END IF; 
+        /* Verifica se existem tarefas de contagem em aberto */
+        IF FN_CONTAGEM_ABERTAS_DCCO(r_produto.nu_ivt, r_produto.cod_prod, r_produto.cod_end) > 0 THEN
+            v_produtos_abertos := v_produtos_abertos || 
+                                c_quebra_linha || 
+                                '<b>Código:</b>' || 
+                                r_produto.cod_prod;
+                                
+        /* Verifica se a conferência do produto está OK */
+        ELSIF FN_CONFERENCIA_OK_DCCO(r_produto.nu_ivt, r_produto.cod_prod, r_produto.cod_end) = 1 THEN
+            v_produtos_conf_ok := v_produtos_conf_ok || 
+                                c_quebra_linha || 
+                                '<b>Código:</b>' || 
+                                r_produto.cod_prod;
+                                
+        /* Gera nova tarefa caso não existam pendências */
+        ELSE
+            /* Gera número da tarefa */
+            STP_GERAR_NUTAREFA_DCCO(v_num_tarefa);
+            
+            /* Insere tarefa de contagem */
+            INSERIR_TGWTAR_DCCO(
+                p_nutarefa  => v_num_tarefa,
+                p_codtarefa => c_tipo_tarefa,
+                p_status    => c_status_ativo,
+                p_codusu    => p_cod_usuario,
+                p_nuivt     => r_produto.nu_ivt,
+                p_pendente  => c_flag_nao,
+                p_resposta  => v_resposta_itt
+            );
+            
+            /* Insere produto na tarefa de contagem */
+            INS_TGWITT_INVT_DCCO(
+                p_nutarefa => v_num_tarefa,
+                p_codprod  => r_produto.cod_prod,
+                p_codend   => r_produto.cod_end,
+                p_resposta => v_resposta_itt
+            );
+            
+            /* Define o inventariante responsável */
+            DefiniInventariante_DCCO(
+                p_nuivt    => r_produto.nu_ivt,
+                p_codprod  => r_produto.cod_prod,
+                p_nutarefa => v_num_tarefa,
+                p_codusu   => p_cod_usuario,
+                p_resposta => v_resposta_uxt
+            );
+            
+            /* Registra produtos adicionados */
+            v_produtos_add := v_produtos_add || 
+                            c_quebra_linha || 
+                            c_quebra_linha || 
+                            'Código: ' || 
+                            r_produto.cod_prod || 
+                            ' <b>Nº TAREFA:</b> ' || 
+                            v_num_tarefa;
+        END IF; 
 
        END LOOP;
 
 
-       -- Construção da mensagem final de retorno
-       IF AUX_PRODUTOS_CTG_ABERTAS IS NOT NULL THEN
-           P_MENSAGEM := 'Os produtos abaixo já possuem tarefas de recontagem em aberto para este inventário:<br>';
-           P_MENSAGEM := P_MENSAGEM || AUX_PRODUTOS_CTG_ABERTAS;
-           P_MENSAGEM := P_MENSAGEM || '<br><br><b>Não foram geradas novas tarefas de recontagem para esses itens.</b>';
-       END IF; 
-
-
-        IF AUX_PRODUTOS_CONF_OK IS NOT NULL THEN
-           P_MENSAGEM := P_MENSAGEM || '<br><br><b>Produtos com conferência OK</b>:<br>';
-           P_MENSAGEM := P_MENSAGEM || AUX_PRODUTOS_CONF_OK;
-        END IF;
-        
-        
-        IF AUX_PRODUTOS_ADICIONADOS IS NOT NULL THEN
-           P_MENSAGEM := P_MENSAGEM || '<br><br><b>Tarefa de recontagem gerada para os produtos abaixo:</b><br>';
-           P_MENSAGEM := P_MENSAGEM || AUX_PRODUTOS_ADICIONADOS;
-       END IF;
-
-
-       IF ERRO_ITT IS NOT NULL THEN
-           P_MENSAGEM := P_MENSAGEM || '<br>' || ERRO_ITT;
-       END IF;
+       /* Monta mensagem de retorno */
+        p_mensagem := NULL;
+    
+    /* Adiciona mensagem de tarefas em aberto */
+    IF v_produtos_abertos IS NOT NULL THEN
+        p_mensagem := 'Os produtos abaixo já possuem tarefas de recontagem em aberto para este inventário:' || 
+                     v_produtos_abertos ||
+                     c_quebra_linha ||
+                     c_quebra_linha ||
+                     '<b>Não foram geradas novas tarefas de recontagem para esses itens.</b>';
+    END IF;
+    
+    /* Adiciona mensagem de produtos conferidos */
+    IF v_produtos_conf_ok IS NOT NULL THEN
+        p_mensagem := p_mensagem || 
+                     c_quebra_linha ||
+                     c_quebra_linha ||
+                     '<b>Produtos com conferência OK</b>:' ||
+                     v_produtos_conf_ok;
+    END IF;
+    
+    /* Adiciona mensagem de novas tarefas */
+    IF v_produtos_add IS NOT NULL THEN
+        p_mensagem := p_mensagem || 
+                     c_quebra_linha ||
+                     c_quebra_linha ||
+                     '<b>Tarefa de recontagem gerada para os produtos abaixo:</b>' ||
+                     v_produtos_add;
+    END IF;
 
 END;
 

5354f61d92ffaf290ea9eada9bbf27da90177bcd
Danilo Fernando <danilo.bossanova@hotmail.com>
2024-11-07 16:09:01 -0300

    :up:  Recontagem divergencia ajustes
    Ajustes. Criado regra que impede que sejam tarefas em duplicidade para produtos que ainda estão com tarefa de contagem abertas. Agora só gera tarefa de contagem para produtos que estao com tarefas finalizadas.

--- diversos/wms/PR_GERAR_TAREFAS_DIVERGENCIA.sql
+++ diversos/wms/PR_GERAR_TAREFAS_DIVERGENCIA.sql
@@ -14,23 +14,26 @@ CREATE OR REPLACE PROCEDURE PR_GERAR_TAREFAS_DIVERGENCIA (
     * @parametro: p_NUIVT - Numero do Inventario. 
     **************************************************************************/
     
-    
 BEGIN
     -- Para cada prédio com divergência
     FOR r_predio IN (
-        WITH DadosAgrupados AS (
-            SELECT 
-                END_RUA_PREDIO,
-                MIN(END_RUA) AS END_RUA,
-                MIN(END_PREDIO) AS END_PREDIO,
-                MIN(NUIVT) AS NUIVT,
-                MIN(CODEMP) AS CODEMP,
-                COUNT(*) AS QTD_PRODUTOS
-            FROM VW_WMSINVT_DIVERGENCIAS
-            WHERE NUIVT = P_NUIVT
-            GROUP BY END_RUA_PREDIO
+        SELECT 
+            END_RUA_PREDIO,
+            COUNT(*) AS QTD_PRODUTOS
+        FROM VW_WMSINVT_DIVERGENCIAS div
+        WHERE div.NUIVT = P_NUIVT
+        -- Verifica se não existem tarefas abertas para os produtos deste prédio
+        AND NOT EXISTS (
+            SELECT 1 
+            FROM TGWTAR tar
+            JOIN TGWITT itt ON tar.NUTAREFA = itt.NUTAREFA
+            WHERE tar.CODTAREFA = 6
+            AND tar.NUIVT = div.NUIVT
+            AND tar.SITUACAO = 'A'
+            AND itt.CODPROD = div.CODPROD
+            AND itt.CODENDORIGEM = div.CODEND
         )
-        SELECT * FROM DadosAgrupados
+        GROUP BY END_RUA_PREDIO
         ORDER BY END_RUA_PREDIO
     ) LOOP
         -- 1. Gerar número da nova tarefa
@@ -39,25 +42,36 @@ BEGIN
         -- 2. Inserir a tarefa
         INSERIR_TGWTAR_DCCO(
             V_NOVA_TAREFA,  -- Número da tarefa gerada
-            6,              -- CODTTAR fixo em 6 Codito da Tarefa de Contagem para Inventario
-            'A',            -- STATUS inicial 'A' - Aberto
+            6,              -- CODTAREFA fixo em 6 Codigo da Tarefa de Contagem para Inventario
+            'A',            -- SITUACAO inicial 'A' - Aberto
             NULL, NULL, NULL, NULL, NULL,
             0,              
-            r_predio.NUIVT, -- NUIVT do INVENTARIO
+            P_NUIVT,        -- NUIVT do INVENTARIO
             NULL, NULL,
             'N',            
             0,              -- CODUSU do SUP que gerou a tarefa
             V_RESPOSTA      -- Variável para resposta
         );
         
-        -- 3. Inserir os itens da tarefa para todos os produtos do prédio que estão com tarefas divergentes
+        -- 3. Inserir os itens da tarefa para todos os produtos do prédio
         FOR r_produto IN (
             SELECT 
-                CODPROD,
-                CODEND
-            FROM VW_WMSINVT_DIVERGENCIAS
-            WHERE NUIVT = P_NUIVT
-            AND END_RUA_PREDIO = r_predio.END_RUA_PREDIO
+                div.CODPROD,
+                div.CODEND
+            FROM VW_WMSINVT_DIVERGENCIAS div
+            WHERE div.NUIVT = P_NUIVT
+            AND div.END_RUA_PREDIO = r_predio.END_RUA_PREDIO
+            -- Não possui tarefa em aberto
+            AND NOT EXISTS (
+                SELECT 1 
+                FROM TGWTAR tar
+                JOIN TGWITT itt ON tar.NUTAREFA = itt.NUTAREFA
+                WHERE tar.CODTAREFA = 6
+                AND tar.NUIVT = div.NUIVT
+                AND tar.SITUACAO = 'A'
+                AND itt.CODPROD = div.CODPROD
+                AND itt.CODENDORIGEM = div.CODEND
+            )
         ) LOOP
             -- Inserir item
             INS_TGWITT_INVT_DCCO(
@@ -69,7 +83,6 @@ BEGIN
         END LOOP;
         
         COMMIT;
-        
     END LOOP;
     
 EXCEPTION
@@ -78,4 +91,3 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('Erro: ' || SQLERRM);
         RAISE;
 END;
-/
